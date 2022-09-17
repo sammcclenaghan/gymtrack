90e63b96388a04fca3c24462e7e9dfd0c1cc0321
Sam McClenaghan <sam@aream.ca>
2022-09-16 22:17:41 -0600

    Made adjustments to database

--- db/schema.rb
+++ db/schema.rb
@@ -17,15 +17,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_11_030559) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "favourites", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "workout_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_favourites_on_user_id"
-    t.index ["workout_id"], name: "index_favourites_on_workout_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -67,8 +58,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_11_030559) do
     t.index ["user_id"], name: "index_workouts_on_user_id"
   end
 
-  add_foreign_key "favourites", "users"
-  add_foreign_key "favourites", "workouts"
   add_foreign_key "workout_exercises", "exercises"
   add_foreign_key "workout_exercises", "workouts"
   add_foreign_key "workout_sets", "workout_exercises"
